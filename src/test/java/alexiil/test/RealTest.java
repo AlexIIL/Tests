109f7148c6aacc34c915d12195f4db13c1395351
AlexIIL <alexj9@me.com>
2017-04-20 22:20:21 +0100

    Fix tests again.

--- src/test/java/alexiil/test/RealTest.java
+++ src/test/java/alexiil/test/RealTest.java
@@ -6,6 +6,6 @@ public class RealTest extends Setup {
     @Test
     public void test() {
         System.out.println("Running RealTest.test()");
-        Assert.assertFalse("The flag was not true! What?", Setup.flag);
+        Assert.assertTrue("The flag was not true! What?", Setup.flag);
     }
 }
